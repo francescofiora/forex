628f691060574bbc917c9939552eb67f0d3f1c49
Francesco Fiora <info@francescofiora.it>
2020-08-23 16:24:28 +0100

    upgrade Unit Tests using JUnit 5

--- src/test/java/it/francescofiora/forex/web/api/ExchangeRateApiTest.java
+++ src/test/java/it/francescofiora/forex/web/api/ExchangeRateApiTest.java
@@ -19,15 +19,16 @@ import it.francescofiora.forex.web.errors.NotFoundException;
 
 import java.net.URI;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = ExchangeRateApi.class)
 public class ExchangeRateApiTest {
 
